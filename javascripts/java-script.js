ffad57138d97e05749cf065dabba414ce9190279
K1f1r1k <ennikiforova@edu.hse.ru>
2022-06-16 21:51:12 +0300

    about

--- javascripts/java-script.js
+++ javascripts/java-script.js
@@ -8,3 +8,34 @@ $(function(){
 $(function(){
 $( ".tickets" ).draggable({ axis: "x" });
 });
+
+$(function(){
+$('.next').on('click', function(){
+	var currentimg = $('.active');
+	var nextimg = currentimg.next();
+
+	if (nextimg.length) {
+		currentimg.removeClass('active').css('z-index', -10);
+		nextimg.addClass('active').css('z-index', 10);
+	}
+});
+});
+
+$(function(){
+$('.prev').on('click', function(){
+	var currentimg = $('.active');
+	var previmg = currentimg.prev();
+
+	if (previmg.length) {
+		currentimg.removeClass('active').css('z-index', -10);
+		previmg.addClass('active').css('z-index', 10);
+	}
+});
+});
+
+$(function(){
+	$('.karta').on('click', function(){
+		$('.footer').css( {'top':'170vh'})
+			$('.screen2').slideDown(500)
+	});
+});
